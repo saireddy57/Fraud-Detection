cba4d483fbaa9b7ecffc6c613bc37339b3a3e0f8
saireddy57 <saireddy111996@gmail.com>
2022-04-22 02:43:18 +0530

    Commited for deplyement via pycharm for creating CI/CD  PIPELINES installed pytest modeule updated flask dashboard and markupsafe

--- tests/test_script.py
+++ tests/test_script.py
@@ -7,15 +7,18 @@ import os
 class TestToPerform(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+        print("self=-----------------------------------appppppppppppppppppppppppp",self.app,app)
 
     def tearDown(self):
         pass
 
     def test_page(self):
         response = self.app.get('/', follow_redirects=True)
+        print("rres---------------------------------------------ponseeeeeeeeeeeeeeeeeeeeeeeee",response,self.app.get('/'))
         print(response)
         self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
+    print("_________________________________________________________name________________________________________________",unittest.main())
     unittest.main()
